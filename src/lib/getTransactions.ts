b46e3252bec5aea7be3b5dd7b22e55c0654409a3
Marcos Salazar <marcos1010521@gmail.com>
2024-10-24 16:20:19 -0400

    add data file

--- src/lib/getTransactions.ts
+++ src/lib/getTransactions.ts
@@ -1,11 +1,6 @@
 import { Transaction } from '@/types';
-import path from 'path';
-import fs from 'fs/promises';
-import { cache } from 'react';
+import transactions from '../../data/transactions.json';
 
-export const getTransactions = cache(async (): Promise<Transaction[]> => {
-  const filePath = path.join(process.cwd(), 'data', 'transactions.json');
-  const data = await fs.readFile(filePath, 'utf-8');
-  const transactions: Transaction[] = JSON.parse(data);
-  return transactions;
-});
+export const getTransactions = async (): Promise<Transaction[]> => {
+  return transactions as Transaction[];
+};
